ba80885655cbae18716969d2c73597c3dec98319
Victor Ogudu <victor.ogudu@gmail.com>
2019-11-26 14:47:47 +0100

    Added AccountController test

---
+++ test/unit/controllers/AccountController.spec.ts
@@ -0,0 +1,49 @@
+import { AccountController } from '../../../src/controller/AccountController';
+import { AccountService } from '../../../src/service/AccountService';
+import { ILoginDto } from '../../../src/dto/ILoginDto';
+import { IRegisterDto } from '../../../src/dto/IRegisterDto';
+import { ITokenData } from '../../../src/dto/ITokenData';
+
+describe('AccountController', () => {
+	let accountController: AccountController;
+
+	const accountService = jest.fn<AccountService, any>(
+		() =>
+			({
+				login: async (loginDto: ILoginDto): Promise<ITokenData> =>
+					({
+						token: '',
+						expiresIn: 90
+					} as ITokenData),
+				register: async (registerDto: IRegisterDto) =>
+					({
+						token: '',
+						expiresIn: 90
+					} as ITokenData)
+			} as AccountService)
+	);
+
+	beforeEach(() => {
+		accountController = new AccountController(accountService());
+	});
+
+	it('Should be able to log user', async () => {
+		const result = await accountController.login({
+			email: 'victor@mail.com',
+			password: '282828nsis'
+		});
+
+		expect(result).toBeTruthy();
+	});
+
+	it('Should be able to register user', async () => {
+		const result = await accountController.register({
+			email: 'victor@mail.com',
+			password: '282828nsis',
+			firstName: 'Victor',
+			lastName: 'Ogudu'
+		});
+
+		expect(result).toBeTruthy();
+	});
+});
