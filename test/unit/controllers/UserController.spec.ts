3b6e48c6ca957049932597759aa2f4885e012f37
Victor Ogudu <victor.ogudu@gmail.com>
2019-11-26 15:43:08 +0100

    Added UserController test

---
+++ test/unit/controllers/UserController.spec.ts
@@ -0,0 +1,64 @@
+import { UserController } from '../../../src/controller/UserController';
+import { UserService } from '../../../src/service/UserService';
+import { IUser } from '../../../src/dto/IUser';
+import { DeleteResult } from 'typeorm';
+import { Users } from '../__mocks__/MockData';
+
+describe('UserController', () => {
+	let userController: UserController;
+	let userService: jest.Mock<UserService, any>;
+
+	beforeAll(() => {
+		userService = jest.fn<UserService, any>(
+			() =>
+				({
+					getAll: async (): Promise<IUser[]> => Users,
+					getOne: async (id: string): Promise<IUser> =>
+						Users.find(x => x.id == id),
+					save: async (User: IUser): Promise<IUser> => User,
+					delete: async (id: string): Promise<DeleteResult> => ({
+						affected: 1,
+						raw: ''
+					})
+				} as UserService)
+		);
+	});
+
+	beforeEach(() => {
+		userController = new UserController(userService());
+	});
+
+	it('Should be able to get all users', async () => {
+		const result = await userController.all();
+
+		expect(result).toHaveLength(3);
+	});
+
+	it('Should be able to get Users by id', async () => {
+		const result = await userController.one('3');
+
+		expect(result).toBeTruthy();
+
+		expect(result.email).toBe(Users[2].email);
+	});
+
+	it('Should be able to save a User', async () => {
+		const result = await userController.save({
+			id: '3',
+			email: 'jane@mail.com',
+			password: 'pass@word1',
+			firstName: 'Jane',
+			lastName: 'Doe'
+		} as IUser);
+
+		expect(result).toBeTruthy();
+		expect(result.id).toBe('3');
+		expect(result.email).toBe('jane@mail.com');
+	});
+
+	it('Should be able to delete a User', async () => {
+		const result = await userController.remove('1');
+
+		expect(result).toBeUndefined();
+	});
+});
